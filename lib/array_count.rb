9b2037a338b064a1f49ae59dbafb154352fdde66
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-05 09:57:39 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,10 @@
 def count_strings(array)
   total = 0 
-  
+  array.count do |index|
+    if index.class == String
+      total += index 
+    end 
+    total 
 end
 
 def count_empty_strings(array)
